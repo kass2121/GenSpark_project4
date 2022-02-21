ecfc9c10225b45787ea9ba0b99b152208c8a4836
Kassaw  Ayalew <kass2121@gmail.com>
2022-02-20 22:58:14 -0500

    Update GuessNumberGameTest.java

--- src/guess_number_game/GuessNumberGameTest.java
+++ src/guess_number_game/GuessNumberGameTest.java
@@ -2,12 +2,18 @@ package guess_number_game;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GuessNumberGameTest {
 
     @Test
-    void mainTest() {
+    void main() {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int num = sc.nextInt();
+        assertTrue(num ==3, String.valueOf(str.equalsIgnoreCase(" ")));
 
     }
 }
