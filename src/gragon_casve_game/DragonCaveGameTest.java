f45a1b265c6779af303f0b83ab87b10de87e2518
Kassaw  Ayalew <kass2121@gmail.com>
2022-02-20 22:56:37 -0500

    Update DragonCaveGameTest.java

--- src/gragon_casve_game/DragonCaveGameTest.java
+++ src/gragon_casve_game/DragonCaveGameTest.java
@@ -1,18 +1,27 @@
 package gragon_casve_game;
 
+import com.sun.tools.javac.Main;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import static org.testng.AssertJUnit.assertTrue;
+
 class DragonCaveGameTest {
+    private Object main;
 
-    @Test
+    @BeforeTest
+
     void mainTest() {
+        Main  main;
+
     }
 
     @Test
-    void displayIntroTest() {
+     void displayIntro() {
+        assertTrue("You are in a land full of dragons.\" + \" In front of you,you see two caves.\"\n" +
+                "                + \" In one cave, the dragon is friendly and will share his treasure with you.\"\n" +
+                "                + \" The other dragons greedy and hungry, and will eat you on sight.\"",true);
+
     }
 
-    @Test
-    void chooseCave() {
-    }
 }
